906d91548b6c7a0adb5f813bfdbedd8f02a70530
bcExpt1123 <activitiesapphelpdesk@gmail.com>
2022-12-22 06:21:13 -0800

    remove submodule

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WebGLMap from './webgl-map-lib';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const map = new WebGLMap({
+  id: '__webgl_map_demo',
+  tileServerURL: 'https://maps.ckochis.com/data/v3/{z}/{x}/{y}.pbf',
+  width: 800,
+  height: 600,
+  center: [-73.9834558, 40.6932723],
+  minZoom: 4,
+  maxZoom: 18,
+  zoom: 13,
+  debug: true,
+  layers: {
+    water: [180, 240, 250, 255],
+    landcover: [202, 246, 193, 255],
+    park: [202, 255, 193, 255],
+    building: [185, 175, 139, 191],
+  }
+});
+console.log(map)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
